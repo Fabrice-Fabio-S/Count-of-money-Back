7f7d8e7d339b66d67cadcbc314d43b4d08687589
fabrice <fabricesumsa2000@gmail.com>
2020-11-13 15:53:14 +0100

    create .env file and set db uri

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ const bodyParser                = require('body-parser');
 const port = process.env.PORT || 3000;
 const mongoose  = require('mongoose');
 const app = express();
+require('dotenv').config()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
-const url = "mongodb+srv://epitech:epitech@cluster0.o59nf.mongodb.net/count-money?retryWrites=true&w=majority";
-
-mongoose.connect( process.env.MONGO_DB_URL || url, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect( process.env.MONGO_DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
 
