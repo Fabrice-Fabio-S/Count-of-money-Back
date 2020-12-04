4989a8faf1ed57b4f5ba805e8d03ff5d79415533
Lionel <lionel@declique.fr>
2020-12-04 19:03:37 +0100

    feat(rss): add RssController and /articles route

--- apiRouter.js
+++ apiRouter.js
@@ -1,17 +1,16 @@
+const express = require("express");
+const UserController = require("./controller/UserController");
+const RssController = require("./controller/RssController");
 
+exports.router = (function () {
+  const apiRouter = express.Router();
 
-const express                   = require('express');
-const UserController             = require('./controller/UserController');
+  apiRouter.route("/register").post(UserController.register);
+  apiRouter.route("/login").post(UserController.login);
+  apiRouter.route("/users/profil").get(UserController.getProfile);
+  apiRouter.route("/users/update/info").get(UserController.updateUserInfo);
+  apiRouter.route("/users/update/crypto").get(UserController.updateCryptoList);
+  apiRouter.route("/articles").get(RssController.getRssFeed);
 
-
-exports.router = (function() {
-    const apiRouter = express.Router();
-
-    apiRouter.route('/register').post(UserController.register);
-    apiRouter.route('/login').post(UserController.login);
-    apiRouter.route('/users/profil').get(UserController.getProfile);
-    apiRouter.route('/users/update/info').get(UserController.updateUserInfo);
-    apiRouter.route('/users/update/crypto').get(UserController.updateCryptoList);
-
-    return apiRouter;
+  return apiRouter;
 })();
