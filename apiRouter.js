5c311d144b612f6ef8c45557af45c5e1aed98bdb
Rodolphe Marbot <rodolphe.marbot@gmail.com>
2020-12-06 19:26:48 +0100

    fetch cryptos

--- apiRouter.js
+++ apiRouter.js
@@ -1,17 +1,18 @@
 
 
 const express                   = require('express');
-const UserController             = require('./controller/UserController');
+const UserController            = require('./controller/UserController');
+const CryptoController          = require("./controller/CryptoController");
 
 
 exports.router = (function() {
     const apiRouter = express.Router();
-
     apiRouter.route('/register').post(UserController.register);
     apiRouter.route('/login').post(UserController.login);
     apiRouter.route('/users/profil').get(UserController.getProfile);
     apiRouter.route('/users/update/info').get(UserController.updateUserInfo);
     apiRouter.route('/users/update/crypto').get(UserController.updateCryptoList);
+    apiRouter.route('/cryptos').get(CryptoController.fetch);
 
     return apiRouter;
 })();
