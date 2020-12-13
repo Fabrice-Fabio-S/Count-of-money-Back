f81f85dabce11835a07de088a2fab04e291499c6
Lionel <lionel@declique.fr>
2020-12-13 15:41:13 +0100

    update(crypoList): change cryptoList type to string

--- config/passport.js
+++ config/passport.js
@@ -2,81 +2,115 @@ const UserModel = require("../models/UserModel");
 const randomstring = require("randomstring");
 const jwt = require("jsonwebtoken");
 const config = require("./config");
-const validator             = require("email-validator");
-const LocalStrategy     = require('passport-local').Strategy;
-const GoogleStrategy     = require('passport-google-oauth20').Strategy;
-require('dotenv').config();
+const validator = require("email-validator");
+const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
-module.exports = function(passport){
-    passport.use(
-        new LocalStrategy({
-            usernameField: 'email',
-            passwordField: 'password'
-        },
-        (email, password, done) => {
-            if(validator.validate(email)){
-                UserModel.getUserByEmail(email)
-                    .then( user => {
-                        if(user) {
-                            UserModel.comparePassword(password, user.password, function(err, isMatch) {
-                                if (err) {
-                                    console.log("err 1"); return done(err); }
-                                if (isMatch) {
-                                    const token = jwt.sign(
-                                        { rdn: randomstring.generate({ length: 26, charset: 'alphanumeric'}) },
-                                        config.secret);
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy(
+      {
+        usernameField: "email",
+        passwordField: "password",
+      },
+      (email, password, done) => {
+        if (validator.validate(email)) {
+          UserModel.getUserByEmail(email)
+            .then((user) => {
+              if (user) {
+                UserModel.comparePassword(
+                  password,
+                  user.password,
+                  function (err, isMatch) {
+                    if (err) {
+                      console.log("err 1");
+                      return done(err);
+                    }
+                    if (isMatch) {
+                      const token = jwt.sign(
+                        {
+                          rdn: randomstring.generate({
+                            length: 26,
+                            charset: "alphanumeric",
+                          }),
+                        },
+                        config.secret
+                      );
 
-                                    const data = {
-                                        userId: user._id,
-                                        email: user.email,
-                                        lastname: user.lastname,
-                                        firstname: user.firstname,
-                                        token: token,
-                                    };
+                      const data = {
+                        userId: user._id,
+                        email: user.email,
+                        lastname: user.lastname,
+                        firstname: user.firstname,
+                        token: token,
+                        cryptoList: user.cryptoList,
+                      };
 
-                                    console.log("data : "+JSON.stringify(data,null,4));
+                      console.log("data : " + JSON.stringify(data, null, 4));
 
-                                    UserModel.setLastToken(user.email,token)
-                                        .then( result => {
-                                            return done(null, data);})
-                                        .catch( err => {
-                                            return done(null, false, {statusCode : 404, message: 'Can not set user token'});})
-                                } else {
-                                    // Invalid password
-                                    return done(null, false, {statusCode : 401, message: 'Wrong Password'});
-                                }
-                            });
-                        }
-                        else {
-                            // This email is not found
-                            return done(null, false, {statusCode : 404, message: 'Email not exits'})
-                        }
-                    })
-                    .catch( err => {
-                        return done(null, false, {statusCode : 500, message: 'Internal error'});
-                    });
-            }else{
-                return done(null, false, {statusCode : 401, message : 'Wrong Email format'});
-            }
-        })
-    );
+                      UserModel.setLastToken(user.email, token)
+                        .then((result) => {
+                          return done(null, data);
+                        })
+                        .catch((err) => {
+                          return done(null, false, {
+                            statusCode: 404,
+                            message: "Can not set user token",
+                          });
+                        });
+                    } else {
+                      // Invalid password
+                      return done(null, false, {
+                        statusCode: 401,
+                        message: "Wrong Password",
+                      });
+                    }
+                  }
+                );
+              } else {
+                // This email is not found
+                return done(null, false, {
+                  statusCode: 404,
+                  message: "Email not exits",
+                });
+              }
+            })
+            .catch((err) => {
+              return done(null, false, {
+                statusCode: 500,
+                message: "Internal error",
+              });
+            });
+        } else {
+          return done(null, false, {
+            statusCode: 401,
+            message: "Wrong Email format",
+          });
+        }
+      }
+    )
+  );
 
-    passport.serializeUser(function(user, done) {
-        done(null, user);
-    });
-    passport.deserializeUser(function(user, done) {
-        done(null, user);
-    });
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
 
-    passport.use(new GoogleStrategy({
+  passport.use(
+    new GoogleStrategy(
+      {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: process.env.CALLBACK_URL,
-        passReToCallback: true
-    },
-        function (request, accessToken, refreshToken, profile, done) {
-            console.log("step-2 : "+JSON.stringify(profile,null,4));
-            return done(null, profile);
-        })
+        passReToCallback: true,
+      },
+      function (request, accessToken, refreshToken, profile, done) {
+        console.log("step-2 : " + JSON.stringify(profile, null, 4));
+        return done(null, profile);
+      }
     )
-}
+  );
+};
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,97 +1,97 @@
-const mongoose  = require('mongoose');
-const bcrypt    = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserModelSchema = mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true
-        },
-        lastname: {
-            type: String,
-            required: true
-        },
-        firstname: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        grade: {
-            type: Number,
-            default: 0,
-            required: true
-        },
-        lastJwtToken: {
-            type: String,
-            required: false
-        },
-        cryptoList: {
-            type: [String],
-            required: false,
-        }
-    },
-);
+const UserModelSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  grade: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  lastJwtToken: {
+    type: String,
+    required: false,
+  },
+  cryptoList: {
+    type: String,
+    required: false,
+  },
+});
 
-UserModelSchema.methods.toJSON = function() {
-    const obj = this.toObject();
-    delete obj.password;
-    delete obj.__v;
-    obj.userId = this._id;
-    return obj;
+UserModelSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.__v;
+  obj.userId = this._id;
+  return obj;
 };
 
-const User = mongoose.model('User', UserModelSchema);
+const User = mongoose.model("User", UserModelSchema);
 module.exports = User;
 
 // Get User by email
 module.exports.getUserByEmail = (_email) => {
-    const query = {email: _email};
-    return User.findOne(query).exec();
+  const query = { email: _email };
+  return User.findOne(query).exec();
 };
 
 // Get User by id
 module.exports.getUserById = (id) => {
-    const query = {_id: id};
-    return User.findOne(query).exec();
+  const query = { _id: id };
+  return User.findOne(query).exec();
 };
 
 // Compare passpord and its hash
-module.exports.comparePassword = function(userPassword, hash, callback){
-    bcrypt.compare(userPassword, hash, function(err, isMatch) {
-        if(err) throw err;
-        callback(null, isMatch);
-    });
+module.exports.comparePassword = function (userPassword, hash, callback) {
+  bcrypt.compare(userPassword, hash, function (err, isMatch) {
+    if (err) throw err;
+    callback(null, isMatch);
+  });
 };
 
 // Set user's token
 module.exports.setLastToken = (_email, lastJwtToken) => {
-    const query = {email: _email};
-    const newvalues = { $set: {lastJwtToken: lastJwtToken } };
-    return User.updateOne(query, newvalues).exec();
+  const query = { email: _email };
+  const newvalues = { $set: { lastJwtToken: lastJwtToken } };
+  return User.updateOne(query, newvalues).exec();
 };
 
 // Set user's grade
 // 0 - user lamda || 1 - admin
 module.exports.setUserGrade = (_email, _grade) => {
-    const query = {email: _email};
-    const newvalues = { $set: {grade: _grade }};
-    return User.updateOne(query, newvalues).exec();
+  const query = { email: _email };
+  const newvalues = { $set: { grade: _grade } };
+  return User.updateOne(query, newvalues).exec();
 };
 
 // Update user info
-module.exports.updateParams = (userId,email,lastname,firstname) => {
-    const query = {_id: userId};
-    const newvalues = { $set: {email: email, lastname: lastname, firstname:firstname } };
-    return User.updateOne(query, newvalues).exec();
+module.exports.updateParams = (userId, email, lastname, firstname) => {
+  const query = { _id: userId };
+  const newvalues = {
+    $set: { email: email, lastname: lastname, firstname: firstname },
+  };
+  return User.updateOne(query, newvalues).exec();
 };
 
 // Update cryptoList
-module.exports.setCryptoList = (userId, crypto)=>{
-    console.log("new pseudo : "+crypto);
-    const query = {_id: userId};
-    const newvalues = {$addToSet: {cryptoList: crypto}};
-    return User.updateOne(query, newvalues)
-}
+module.exports.setCryptoList = (userId, crypto) => {
+  console.log("new pseudo : " + crypto);
+  const query = { _id: userId };
+  const newvalues = { $set: { cryptoList: crypto } };
+  return User.updateOne(query, newvalues);
+};
